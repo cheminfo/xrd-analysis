93a1f67b3edb04b1acc82bf855b786313b18f85b
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-07 15:12:06 +0200

    fix: fixed missing prettier issue

--- src/from/__tests__/fromPowDLLXY.test.js
+++ src/from/__tests__/fromPowDLLXY.test.js
@@ -14,12 +14,8 @@ describe('fromXY', () => {
     let diffractogram = analysis.getXYSpectrum();
     expect(diffractogram.meta.userName).toBe('Lab Manager');
     expect(diffractogram.variables.x.data[0]).toBe(10);
-    expect(diffractogram.variables.y.data[1]).toBe(
-      -0.0755227112146954,
-    );
-    expect(diffractogram.variables.x.data[3448]).toBe(
-      70.0050142705441,
-    );
+    expect(diffractogram.variables.y.data[1]).toBe(-0.0755227112146954);
+    expect(diffractogram.variables.x.data[3448]).toBe(70.0050142705441);
     expect(diffractogram.variables.y.data[3448]).toBe(0);
     expect(diffractogram.variables.x.data).toHaveLength(3449);
     expect(diffractogram.variables.y.data).toHaveLength(3449);
